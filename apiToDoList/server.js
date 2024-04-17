8891eb1164e1e22234ab5bef0a03819afb8ce23f
klemaire60 <lemaire.kevin6020@gmail.com>
2024-04-17 16:56:35 +0200

    Back Connexion fonctionnelle

--- apiToDoList/server.js
+++ apiToDoList/server.js
@@ -9,7 +9,8 @@ const connection = require('./db');
 
 const app = express();
 const port = 3000;
-const jwtKey = "L'énergie créative est contagieuse, passez-la autour."
+const jwtKey = "key";
+const bcryptKey = "key";
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -19,27 +20,27 @@ app.post('/register' , (req, res) => {
     
     const {mail, password} = req.body;
     if(!mail || !password) return res.status(400).json({ message : "L'Email ou le mot de passe est manquant !"});
-
+    
     if(!validator.isEmail(mail)) return res.status(400).json({ message : "L'email n'est pas valide"});
-
+    
     const sqlVerif = `SELECT mail FROM User WHERE mail = ?`;
     
     connection.query(sqlVerif, [mail], (err, results) => {
-
+        
         if(err) {
             console.error("Erreur SQL lors de la verification du mail\nerreur : ", err)
             return res.status(500).json({ message: 'Erreur lors de la création du compte'});
         }
         
         if(results.length === 0) {
-            bcrypt.hash(password, 10, (hashErr, hashedPassword) => {
+            bcrypt.hash(password, bcryptKey, (hashErr, hashedPassword) => {
                 if (hashErr) {
                     console.error('Erreur lors du hachage du mot de passe :', hashErr);
                     return res.status(500).json({ message: "Erreur lors de la création du compte" });
                 }
-
+                
                 let token = jwt.sign({ mail }, jwtKey);
-
+                
                 const sqlNewUser = `INSERT INTO User (mail, password, token) VALUES (?, ?, ?)`;
                 connection.query(sqlNewUser, [mail, hashedPassword, token], (sqlErr) => {
                     if (sqlErr) {
@@ -50,10 +51,50 @@ app.post('/register' , (req, res) => {
                 });
             });
         } else {
-            return res.json({message : "Email déjà utilisé"});
+            return res.json(
+                {
+                    message : "Email déjà utilisé",
+                    token : token
+                }
+            );
         }
     })
 });
+
+app.post('/login', (req, res) => {
+    const {mail, password} = req.body;
+    
+    if(!mail || !password) return res.status(400).json({ message : "L'Email ou le mot de passe est manquant !"});
+    
+    if(!validator.isEmail(mail)) return res.status(400).json({ message : "L'email n'est pas valide"});
+    
+    const sqlLogin = 'SELECT password FROM User WHERE mail = ?'
+    
+    connection.query(sqlLogin, [mail], (err, results) => {
+        if(err) {
+            console.error('Erreur lors de la requete sql login\nErreur : ', err);
+            return res.status(500).json({message : "erreur lors de la connexion"});
+        }
+        
+        if(results.length === 0) {
+            return res.json({message : "Email incorrect"})
+        }
+        
+        const password = bcrypt.compare(results[0].password, bcryptKey)
+        
+        if(!password) {
+            return res.json({message : "Mot de passe incorrect"});
+        } else {
+            let token = jwt.sign({ mail }, jwtKey);
+            return res.json(
+                {
+                    message : "Connexion réussie", 
+                    token : token
+                }
+            )
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`Le serveur est en écoute sur le port ${port}`);
