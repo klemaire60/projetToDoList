69a469a26be0aa3e253f8e71a270679d0fcd7d12
klemaire60 <lemaire.kevin6020@gmail.com>
2024-04-18 11:46:21 +0200

    Ajout de tâche fonctionnelle

--- apiToDoList/server.js
+++ apiToDoList/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const validator = require('validator');
+const rateLimit = require('express-rate-limit');
 
 const connection = require('./db');
 
@@ -15,6 +16,13 @@ const bcryptKey = "key";
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
 
 app.post('/register' , (req, res) => {
     
@@ -51,7 +59,7 @@ app.post('/register' , (req, res) => {
                 });
             });
         } else {
-            return res.json(
+            return res.status(409).json(
                 {
                     message : "Email déjà utilisé",
                     token : token
@@ -65,7 +73,6 @@ app.post('/login', (req, res) => {
     const {mail, password} = req.body;
     
     if(!mail || !password) return res.status(400).json({ message : "L'Email ou le mot de passe est manquant !"});
-    
     if(!validator.isEmail(mail)) return res.status(400).json({ message : "L'email n'est pas valide"});
     
     const sqlLogin = 'SELECT password FROM User WHERE mail = ?'
@@ -77,24 +84,55 @@ app.post('/login', (req, res) => {
         }
         
         if(results.length === 0) {
-            return res.json({message : "Email incorrect"})
+            return res.status(409).json({message : "Email incorrect"})
         }
         
         const password = bcrypt.compare(results[0].password, bcryptKey)
         
         if(!password) {
-            return res.json({message : "Mot de passe incorrect"});
+            return res.status(409).json({message : "Mot de passe incorrect"});
         } else {
             let token = jwt.sign({ mail }, jwtKey);
-            return res.json(
+            return res.status(200).json(
                 {
                     message : "Connexion réussie", 
                     token : token
                 }
-            )
+            );
         }
     })
 })
+
+app.post('/addTask', (req, res) => {
+    const {taskName, userToken} = req.body;
+
+    if(!taskName) return res.status(400).json({message : "Un nom est requis"});
+    if(!userToken) return res.status(400).json({message : "Le token du user est requis"});
+
+    const sqlTaskVerif = 'SELECT * FROM Task WHERE name = ? AND idUser  = (SELECT id FROM User WHERE token = ?)';
+    connection.query(sqlTaskVerif, [taskName, userToken], (err, results) => {
+
+        if(err) {
+            console.error('Erreur lors de la requete SQL TaskVerif\n erreur : ', err);
+            return res.status(500).json({message : "Erreur lors de la création de la tâche"});
+        }
+
+        if(results.length === 0) {
+            const sqlAddTask = 'INSERT INTO Task (name, idUser) VALUES (?, (SELECT id FROM User WHERE token = ?))';
+        
+            connection.query(sqlAddTask, [taskName, userToken], (err) => {
+                if(err) {
+                    console.error('Erreur lors de la requete SQL addTask\n erreur : ', err);
+                    return res.status(500).json({message : "Erreur lors de la création de la tâche"});
+                }
+
+                res.status(200).json({message : "La tâche à bien été ajoutée"})
+            })
+        } else {
+            return res.status(409).json({message : "Vous avez déjà enregistré cette tâche"})
+        }
+    })
+});
 
 app.listen(port, () => {
     console.log(`Le serveur est en écoute sur le port ${port}`);
